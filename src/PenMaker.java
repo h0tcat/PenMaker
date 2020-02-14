49c141b9fd4b3cda5c351c4e38d2154804fdb2f1
h0tcat <zer0busamx@yahoo.co.jp>
2020-02-14 10:17:28 +0900

    First Commit

---
+++ src/PenMaker.java
@@ -0,0 +1,44 @@
+import javafx.scene.image.Image;
+import nori.soft.util.detailsPen;
+
+import java.io.*;
+
+public class PenMaker {
+    public static void main(String[] args) {
+        if (!(args.length == 2)) {
+            System.out.println("Usage: PenMaker imageFile outputFileName");
+        } else {
+            System.out.println("Build nori file...");
+            FileOutputStream fileOutputStream = null;
+            try {
+                fileOutputStream = new FileOutputStream(args[1]+".nori");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            ObjectOutputStream objOutputStream = null;
+            try {
+                objOutputStream = new ObjectOutputStream(fileOutputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            detailsPen penPlugin = new detailsPen(new Image(new File(args[0]).toURI().toString()));
+            try {
+                objOutputStream.writeObject(penPlugin);
+                System.out.println("D 0 N E !");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    objOutputStream.flush();
+                    objOutputStream.close();
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ src/nori/soft/util/detailsPen.java
@@ -0,0 +1,19 @@
+package nori.soft.util;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+import java.io.Serializable;
+
+public class detailsPen implements Serializable {
+    private static final long detailsPenSerialPen = 10L;
+    Image drawBrushImage;
+
+    public detailsPen(Image drawBrushImage) {
+        this.drawBrushImage = drawBrushImage;
+    }
+
+    public void draw(GraphicsContext graphCont, double drawX, double drawY) {
+        graphCont.drawImage(this.drawBrushImage, drawX, drawY);
+    }
+}
